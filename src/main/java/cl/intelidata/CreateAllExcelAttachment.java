4968bab4c0fdf773dd1d4f2ce92a15a4a3a30d35
mazebeat <diego.pintod@gmail.com>
2015-03-05 14:41:44 -0300

    0.0.4

--- src/main/java/cl/intelidata/CreateAllExcelAttachment.java
+++ src/main/java/cl/intelidata/CreateAllExcelAttachment.java
@@ -1,7 +1,17 @@
 /**
- * 
+ *
  */
 package cl.intelidata;
+
+import cl.intelidata.utils.ExcelLib;
+import cl.intelidata.utils.FileLib;
+import cl.intelidata.utils.StringLib;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.*;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -12,25 +22,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.IndexedColors;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFCellStyle;
-import org.apache.poi.xssf.usermodel.XSSFFont;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import cl.intelidata.utils.ExcelLib;
-import cl.intelidata.utils.FileLib;
-import cl.intelidata.utils.StringLib;
-
 /**
  * @author DIEGOPC
- * 
  */
 public class CreateAllExcelAttachment {
 
@@ -40,8 +33,7 @@ public class CreateAllExcelAttachment {
 	public static void main(String[] args) throws IOException {
 		// Valida la cantidad de argumentos de entrada
 		if (args.length != 2) {
-			StringLib
-					.generateWarning("The number of parameters is incorrect: [fileToRead] [srcDestiny]");
+			StringLib.generateWarning("The number of parameters is incorrect: [fileToRead] [srcDestiny]");
 			System.exit(0);
 		}
 
@@ -61,13 +53,13 @@ public class CreateAllExcelAttachment {
 
 	/**
 	 * Procesa la información obtenida del archivo matriz
-	 * 
+	 *
 	 * @param srcFileRead
+	 *
 	 * @throws IOException
 	 * @throws FileNotFoundException
 	 */
-	public static void process(File srcFileRead) throws IOException,
-			FileNotFoundException {
+	public static void process(File srcFileRead) throws IOException, FileNotFoundException {
 		try {
 			int countRows = 1;
 			String mailAdmin, dateProcess = null;
@@ -76,12 +68,13 @@ public class CreateAllExcelAttachment {
 			XSSFSheet sheet = workbook.getSheetAt(0);
 
 			List<String> listMails = new ArrayList<String>(
-					createListMails(sheet));
+					createListMails(sheet)
+			);
 
 			Iterator<String> it = listMails.iterator();
 
 			while (it.hasNext()) {
-				mailAdmin = (String) it.next();
+				mailAdmin = (String)it.next();
 
 				XSSFWorkbook workbook2 = new XSSFWorkbook();
 				XSSFSheet sheet2 = workbook2.createSheet();
@@ -91,33 +84,42 @@ public class CreateAllExcelAttachment {
 					if (i == 0) {
 						XSSFRow fila = sheet2.createRow(i);
 
-						StringLib.generateInfo("Creating header for "
-								+ mailAdmin + " ...");
+						StringLib.generateInfo(
+								"Creating header for " + mailAdmin + " ..."
+						);
 
 						if (row.getCell(0).getCellType() != Cell.CELL_TYPE_BLANK) {
 							XSSFCell cell = fila.createCell(0);
 							row.getCell(0).setCellType(Cell.CELL_TYPE_STRING);
-							cell.setCellValue(ExcelLib.readCellValue(row
-									.getCell(0)));
+							cell.setCellValue(
+									ExcelLib.readCellValue(
+											row.getCell(0)
+									)
+							);
 							sheet2.autoSizeColumn(0);
 
 							XSSFCellStyle style = workbook2.createCellStyle();
-							style.setFillForegroundColor(IndexedColors.DARK_BLUE
-									.getIndex());
+							style.setFillForegroundColor(
+									IndexedColors.DARK_BLUE.getIndex()
+							);
 							style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
 
 							style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-							style.setBottomBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setBottomBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-							style.setLeftBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setLeftBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-							style.setRightBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setRightBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-							style.setTopBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setTopBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							cell.setCellStyle(style);
 
 							XSSFFont font = workbook2.createFont();
@@ -128,27 +130,35 @@ public class CreateAllExcelAttachment {
 						if (row.getCell(4).getCellType() != Cell.CELL_TYPE_BLANK) {
 							XSSFCell cell = fila.createCell(1);
 							row.getCell(4).setCellType(Cell.CELL_TYPE_STRING);
-							cell.setCellValue(ExcelLib.readCellValue(row
-									.getCell(4)));
+							cell.setCellValue(
+									ExcelLib.readCellValue(
+											row.getCell(4)
+									)
+							);
 							sheet2.autoSizeColumn(1);
 
 							XSSFCellStyle style = workbook2.createCellStyle();
-							style.setFillForegroundColor(IndexedColors.DARK_BLUE
-									.getIndex());
+							style.setFillForegroundColor(
+									IndexedColors.DARK_BLUE.getIndex()
+							);
 							style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
 
 							style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-							style.setBottomBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setBottomBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-							style.setLeftBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setLeftBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-							style.setRightBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setRightBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-							style.setTopBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setTopBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							cell.setCellStyle(style);
 
 							XSSFFont font = workbook2.createFont();
@@ -159,27 +169,35 @@ public class CreateAllExcelAttachment {
 						if (row.getCell(5).getCellType() != Cell.CELL_TYPE_BLANK) {
 							XSSFCell cell = fila.createCell(2);
 							row.getCell(5).setCellType(Cell.CELL_TYPE_STRING);
-							cell.setCellValue(ExcelLib.readCellValue(row
-									.getCell(5)));
+							cell.setCellValue(
+									ExcelLib.readCellValue(
+											row.getCell(5)
+									)
+							);
 							sheet2.autoSizeColumn(2);
 
 							XSSFCellStyle style = workbook2.createCellStyle();
-							style.setFillForegroundColor(IndexedColors.DARK_BLUE
-									.getIndex());
+							style.setFillForegroundColor(
+									IndexedColors.DARK_BLUE.getIndex()
+							);
 							style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
 
 							style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-							style.setBottomBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setBottomBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-							style.setLeftBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setLeftBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-							style.setRightBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setRightBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-							style.setTopBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setTopBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							cell.setCellStyle(style);
 
 							XSSFFont font = workbook2.createFont();
@@ -190,27 +208,35 @@ public class CreateAllExcelAttachment {
 						if (row.getCell(6).getCellType() != Cell.CELL_TYPE_BLANK) {
 							XSSFCell cell = fila.createCell(3);
 							row.getCell(6).setCellType(Cell.CELL_TYPE_STRING);
-							cell.setCellValue(ExcelLib.readCellValue(row
-									.getCell(6)));
+							cell.setCellValue(
+									ExcelLib.readCellValue(
+											row.getCell(6)
+									)
+							);
 							sheet2.autoSizeColumn(3);
 
 							XSSFCellStyle style = workbook2.createCellStyle();
-							style.setFillForegroundColor(IndexedColors.DARK_BLUE
-									.getIndex());
+							style.setFillForegroundColor(
+									IndexedColors.DARK_BLUE.getIndex()
+							);
 							style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
 
 							style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-							style.setBottomBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setBottomBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-							style.setLeftBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setLeftBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-							style.setRightBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setRightBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-							style.setTopBorderColor(IndexedColors.BLACK
-									.getIndex());
+							style.setTopBorderColor(
+									IndexedColors.BLACK.getIndex()
+							);
 							cell.setCellStyle(style);
 
 							XSSFFont font = workbook2.createFont();
@@ -247,117 +273,144 @@ public class CreateAllExcelAttachment {
 						StringLib.generateInfo("Header created successfully");
 					} else {
 						if (i == 1) {
-							StringLib.generateInfo("Creating body " + mailAdmin
-									+ " ...");
+							StringLib.generateInfo(
+									"Creating body " + mailAdmin + " ..."
+							);
 						}
 
-						if (row.getCell(1).getStringCellValue()
-								.equalsIgnoreCase(mailAdmin)) {
+						if (row.getCell(1).getStringCellValue().equalsIgnoreCase(mailAdmin)) {
 
 							XSSFRow fila = sheet2.createRow(countRows);
 
 							if (row.getCell(0).getCellType() != Cell.CELL_TYPE_BLANK) {
 								XSSFCell cell = fila.createCell(0);
 								row.getCell(0).setCellType(
-										Cell.CELL_TYPE_STRING);
-								cell.setCellValue(ExcelLib.readCellValue(row
-										.getCell(0)));
+										Cell.CELL_TYPE_STRING
+								);
+								cell.setCellValue(
+										ExcelLib.readCellValue(
+												row.getCell(0)
+										)
+								);
 								sheet2.autoSizeColumn(0);
 
-								XSSFCellStyle style = workbook2
-										.createCellStyle();
+								XSSFCellStyle style = workbook2.createCellStyle();
 								style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-								style.setBottomBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setBottomBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-								style.setLeftBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setLeftBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-								style.setRightBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setRightBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-								style.setTopBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setTopBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								cell.setCellStyle(style);
 							}
 
 							if (row.getCell(4).getCellType() != Cell.CELL_TYPE_BLANK) {
 								XSSFCell cell = fila.createCell(1);
 								row.getCell(4).setCellType(
-										Cell.CELL_TYPE_STRING);
-								cell.setCellValue(ExcelLib.readCellValue(row
-										.getCell(4)));
+										Cell.CELL_TYPE_STRING
+								);
+								cell.setCellValue(
+										ExcelLib.readCellValue(
+												row.getCell(4)
+										)
+								);
 								sheet2.autoSizeColumn(1);
 
-								XSSFCellStyle style = workbook2
-										.createCellStyle();
+								XSSFCellStyle style = workbook2.createCellStyle();
 								style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-								style.setBottomBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setBottomBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-								style.setLeftBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setLeftBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-								style.setRightBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setRightBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-								style.setTopBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setTopBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								cell.setCellStyle(style);
 							}
 
 							if (row.getCell(5).getCellType() != Cell.CELL_TYPE_BLANK) {
 								XSSFCell cell = fila.createCell(2);
 								row.getCell(5).setCellType(
-										Cell.CELL_TYPE_STRING);
-								cell.setCellValue(ExcelLib.readCellValue(row
-										.getCell(5)));
+										Cell.CELL_TYPE_STRING
+								);
+								cell.setCellValue(
+										ExcelLib.readCellValue(
+												row.getCell(5)
+										)
+								);
 								sheet2.autoSizeColumn(2);
 
-								XSSFCellStyle style = workbook2
-										.createCellStyle();
+								XSSFCellStyle style = workbook2.createCellStyle();
 								style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-								style.setBottomBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setBottomBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-								style.setLeftBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setLeftBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-								style.setRightBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setRightBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-								style.setTopBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setTopBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								cell.setCellStyle(style);
 							}
 
 							if (row.getCell(6).getCellType() != Cell.CELL_TYPE_BLANK) {
 								XSSFCell cell = fila.createCell(3);
 								row.getCell(6).setCellType(
-										Cell.CELL_TYPE_STRING);
-								cell.setCellValue(ExcelLib.readCellValue(row
-										.getCell(6)));
+										Cell.CELL_TYPE_STRING
+								);
+								cell.setCellValue(
+										ExcelLib.readCellValue(
+												row.getCell(6)
+										)
+								);
 								sheet2.autoSizeColumn(3);
 
-								XSSFCellStyle style = workbook2
-										.createCellStyle();
+								XSSFCellStyle style = workbook2.createCellStyle();
 								style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-								style.setBottomBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setBottomBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-								style.setLeftBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setLeftBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-								style.setRightBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setRightBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-								style.setTopBorderColor(IndexedColors.BLACK
-										.getIndex());
+								style.setTopBorderColor(
+										IndexedColors.BLACK.getIndex()
+								);
 								cell.setCellStyle(style);
 							}
 
-							dateProcess = ExcelLib
-									.readCellValue(row.getCell(0));
+							dateProcess = ExcelLib.readCellValue(row.getCell(0));
 
 							// for (int c = 0; c < 7; c++) {
 							// XSSFCell cell = fila.createCell(c);
@@ -394,8 +447,9 @@ public class CreateAllExcelAttachment {
 				StringLib.generateInfo("Body created successfully");
 
 				if (dateProcess != null) {
-					String nameFile = FileLib.createNameFile(mailAdmin,
-							dateProcess);
+					String nameFile = FileLib.createNameFile(
+							mailAdmin, dateProcess
+					);
 					FileLib.createFile(nameFile, workbook2);
 				}
 			}
@@ -409,8 +463,9 @@ public class CreateAllExcelAttachment {
 	/**
 	 * Crea una lista con valores unicos con el campo Correo_Administrador del
 	 * archivo matriz
-	 * 
+	 *
 	 * @param sheet
+	 *
 	 * @return
 	 */
 	public static List<String> createListMails(Sheet sheet) {
